c54a12587be405d41205a913896354e06e1593dd
amalnathsathyan <amalnathsathyan@gmail.com>
2026-01-18 17:44:03 +0530

    Created Metadata and got URI

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../turbin3-wallet.json"
+import wallet from "../dev-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -17,27 +17,28 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        const image = "https://gateway.irys.xyz/8T4E6oriR4ByCiZVhbDk4Y2AWBpBeXh7XZHChogXh7cp"
+        const metadata = {
+            name: "RUG-RUG",
+            symbol: "RUGY",
+            description: "Huge Rug",
+            image: image,
+            attributes: [
+                {trait_type: 'rug-type', value: 'gene'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your metadata URI: ", myUri);
+        // Your metadata URI:  https://gateway.irys.xyz/6abobGpNC5p1E8LKWxoxuj76LtotJFAjgyDqqPT2FMe8
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
