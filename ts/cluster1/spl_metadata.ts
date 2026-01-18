526fbf9a7e4a0665af57a95ce371536c4d6cd060
amalnathsathyan <amalnathsathyan@gmail.com>
2026-01-18 16:42:30 +0530

    added metadata to the created mint

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../turbin3-wallet.json"
+import wallet from "../dev-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("3PSaL7kB7FCoT7ZpikoocdmiVhEmwwG2DsfZEJZm9oRw")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,28 +21,40 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint: mint,
+            mintAuthority: signer,
+            payer: signer,
+            updateAuthority:signer,
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name:"COOKIES",
+            symbol:"COOKIES",
+            uri:"https://ipfs.io/ipfs/bafkreihvc3gd3lolfb4vdrqwgwnedl4jwihh5rhbsca5hxdlq56iivsxj4",
+            sellerFeeBasisPoints:0,
+            creators:null,
+            collection:null,
+            uses:null
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data: data,
+            isMutable: false,
+            collectionDetails:null
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
+        //tx sig : 3ryjGR3g2T8jA6vB8x4Qcfjmof1cc1vEEmvMj1jtUrhg8198xhSfvHqcXyCV8UkwNxZWHrCNEUMHDJJoVtoq2Y9v
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
